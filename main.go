6aec20190e583b9dab76d5385eef4a257196798f
Roshan Sharma <reprogram46@gmail.com>
2020-04-16 21:42:26 +0530

    feat: Accept URL as query parameter
    This state works with absolute path only.

--- main.go
+++ main.go
@@ -2,17 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+// DispatchRequest returns response received from target resource
+func DispatchRequest(w http.ResponseWriter, r *http.Request) {
+	// params := mux.Vars(r) // Need to embed optional slash parsinfg in URL
+	params, ok := r.URL.Query()["u"]
+	if !ok || len(params) == 0 {
+		fmt.Fprintf(w, "Target URL invalid or not specified.")
+	}
+	targetURL := params[0]
+
+	fmt.Println("PING", targetURL) // DEV
+	// TBD: Validate `targetURL` structure in preflight request (1).
+	// TBD: Allow more params for customized functionality (2).
+
+	response, err := http.Get(targetURL)
+	if err != nil {
+		panic(err) // Maybe use log.Fatal() instead?
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	fmt.Fprintln(w, string(body))
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Mux!")
-		w.WriteHeader(http.StatusOK)
-	}).Methods("GET")
-	fmt.Println("Listening on localhost:5000")
-	http.ListenAndServe(":5000", r)
+
+	// r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, `To apply this proxy, suffix the desired URL to this
+	// 	URL. However, this may throw an error if the URL is invalid.`)
+	// }).Methods("GET")
+
+	r.HandleFunc("/", DispatchRequest).Methods("GET")
+
+	fmt.Println("Listeing...")
+	log.Fatal(http.ListenAndServe(":8001", r))
 }
