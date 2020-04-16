e5d554f468fcaf9cc3fb70e1d210c400a6f0a701
Roshan Sharma <reprogram46@gmail.com>
2020-04-16 22:24:17 +0530

    fix: Declare /proxy to set proxy
    This works locally for valid URLs. Invalid URLs cause dirty logs.

--- main.go
+++ main.go
@@ -12,13 +12,16 @@ import (
 // DispatchRequest returns response received from target resource
 func DispatchRequest(w http.ResponseWriter, r *http.Request) {
 	// params := mux.Vars(r) // Need to embed optional slash parsinfg in URL
-	params, ok := r.URL.Query()["u"]
-	if !ok || len(params) == 0 {
-		fmt.Fprintf(w, "Target URL invalid or not specified.")
-	}
-	targetURL := params[0]
-
+	params := r.URL.Query()
+	targetURL := params.Get("u")
 	fmt.Println("PING", targetURL) // DEV
+
+	if targetURL == "" {
+		fmt.Fprintf(w, "Target URL not specified.")
+		log.Fatal("Exiting request. URL not specified.")
+		return
+	}
+
 	// TBD: Validate `targetURL` structure in preflight request (1).
 	// TBD: Allow more params for customized functionality (2).
 
@@ -34,12 +37,12 @@ func DispatchRequest(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/help", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, ` To apply this proxy, suffix the desired URL to this
 URL. However, this may throw an error if the URL is invalid.`)
 	}).Methods("GET")
 
-	r.HandleFunc("/", DispatchRequest).Methods("GET")
+	r.HandleFunc("/proxy", DispatchRequest).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8001", r))
 }
